5190dd195628cd8537cca162b0d0abd276c96837
Nam Đặng <dangquocnam1119@gmail.com>
2023-10-22 20:35:07 +0700

    sua thanh tep co bo cuc hop ly

--- ql_dang_nhap/ql_dangnhap.js
+++ ql_dang_nhap/ql_dangnhap.js
@@ -1,8 +1,8 @@
 function validateForm() {
     var username = document.getElementById('uname').value;
     var password = document.getElementById('pwd').value;
-    if (username === 'tendangnhap' && password === 'matkhau') {
-        window.location.href = 'trangchu.html';
+    if (username === 'quanly' && password === 'password') {
+        window.location.href = '/trang_chu/trangchu.html';
         return false;
     } else {
         alert('Tên tài khoản hoặc mật khẩu không đúng. Vui lòng thử lại.');
