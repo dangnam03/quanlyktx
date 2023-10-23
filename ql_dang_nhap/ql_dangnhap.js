396b38ebbb668b51f1c10d1a9c8f56a5ead817fe
Nam Đặng <dangquocnam1119@gmail.com>
2023-10-24 00:03:09 +0700

    fix dang nhap

--- ql_dang_nhap/ql_dangnhap.js
+++ ql_dang_nhap/ql_dangnhap.js
@@ -2,7 +2,7 @@ function validateForm() {
     var username = document.getElementById('uname').value;
     var password = document.getElementById('pwd').value;
     if (username === 'quanly' && password === 'password') {
-        window.location.href = '/trang_chu/trangchu.html';
+        window.location.href = '/sau_khi_ql_dn/saukhi_ql_dn.html';
         return false;
     } else {
         alert('Tên tài khoản hoặc mật khẩu không đúng. Vui lòng thử lại.');
--- sv_dang_nhap/sv_dangnhap.js
+++ sv_dang_nhap/sv_dangnhap.js
@@ -2,7 +2,7 @@ function validateForm() {
     var username = document.getElementById('uname').value;
     var password = document.getElementById('pwd').value;
     if (username === 'tendangnhap' && password === 'matkhau') {
-        window.location.href = '/trang_chu/trangchu.html';
+        window.location.href = '/sau_khi_sv_dn/saukhi_sv_dn.html';
         return false;
     } else {
         alert('Tên tài khoản hoặc mật khẩu không đúng. Vui lòng thử lại.');
