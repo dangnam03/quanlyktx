024849ced70f2d3a0a63949902e0cc918cb5e194
phv23 <146416756+phv23@users.noreply.github.com>
2023-10-24 13:44:43 +0700

    Update BTL2.js

--- BTL2.js
+++ BTL2.js
@@ -1,28 +1,27 @@
-let check_phone = /^\(\d{2,4}\)[\s\.-]\d{3}[\s\.-]\d{3}$/;
-let check_email = /^([\w-]+(\?\:\.[\w-]+)*)@((\?\:[\w-]+\.)*\w[\w-]{0-66})\.([a-z]{2,6}(\?\:\.[a-z]{2})?)$/;
-function checknull(txt) {
-  if (txt.value.length === 0)
-    return true;
-  else
-    return false;
-}
-function isInterger(txt) {
-  if ((!isNaN(txt.value)) && (parseInt(txt.value) === Number(txt.value)))
-    return true;
-  else
-    return false;
-}
-function notcheck(radio) {
-  let rt = true;
-  for (i = 0; i < radio.length; i++)
-    if (radio[i].checked)
-      rt = false;
-  return rt;
-}
-function StringMatch(txt, reg) {
-  return reg.test(txt.value);
-}
-function validForm(myForm) {
+document.getElementById("submit").onclick = function (myForm) {
+  myForm.preventDefault(); // Ngăn chặn gửi biểu mẫu mặc định
+  function checknull(txt) {
+      return txt.value.trim() === "";
+  }
+
+  function notcheck(radio) {
+      for (let i = 0; i < radio.length; i++) {
+          if (radio[i].checked) {
+              return false;
+          }
+      }
+      return true;
+  }
+
+  function StringMatch(txt, reg) {
+      return reg.test(txt.value);
+  }
+
+  function isValidYear(year) {
+      const currentYear = new Date().getFullYear();
+      return year >= 1950 && year <= currentYear;
+  }
+
   let name = document.getElementById("nameInput");
   let studentID = document.getElementById("studentIdInput");
   let phone = document.getElementById("phoneInput");
@@ -34,27 +33,53 @@ function validForm(myForm) {
   let clas = document.getElementById("classInput");
   let id = document.getElementById("IDInput");
   let dctt = document.getElementById("dcttInput");
+
   if (name.value === "" || studentID.value === "" || phone.value === "" || address.value === "" || email.value === "" ||
-    uyear.value === "" || khoa.value === "" || nganh.value === "" || clas.value === "" || id.value === "" ||
-    dctt.value === "") {
-    alert("Thiếu thông tin");
-    return;
-  }
-  
-  if(!(StringMatch(phoneInput,check_phone))){
-    alert("Sai số điện thoại");
-    return;   
+  uyear.value === "" || khoa.value === "" || nganh.value === "" || clas.value === "" || id.value === "" ||
+  dctt.value === "") {
+  alert("Thiếu thông tin");
+  return;
+}
+
+  let genderInput = document.getElementsByName("genderInput");
+  if (notcheck(genderInput)) {
+      alert("Chọn giới tính");
+      return;
   }
 
-  let y = notcheck(genderInput);
-  if (y === true) {
-    alert("Chọn giới tính");
-    return;   
+  let QdcctInput = document.getElementsByName("QdcctInput");
+  if (notcheck(QdcctInput)) {
+      alert("Chọn địa chỉ thường trú của người thân");
+      return;
   }
 
-  let z = notcheck(QdcttInput);
-  if (z === true) {
-    alert("Chọn địa chỉ thường trú của người thân");
-    return;    
+  if ((StringMatch(phone, check_phone)) === false) {
+      alert("Sai định dạng số điện thoại");
+      return;
   }
+
+  if ((!StringMatch(email, check_email))=== false) {
+      alert("Sai định dạng e-mail");
+      return;
+  }
+
+  if (!StringMatch(uyear, check_uyear)) {
+      alert("Sai định dạng Khóa");
+      return;
+  }
+
+  const bDate = document.getElementById("bDateInput").value;
+  if (!bDate) {
+      alert("Vui lòng nhập ngày sinh");
+      return;
+  }
+
+  const birthYear = new Date(bDate).getFullYear();
+  if (!isValidYear(birthYear)) {
+      alert("Năm sinh phải từ 1950 đến " + currentYear);
+      return;
+  }
+
+  alert("Đăng ký thành công");
+  // Thêm mã JavaScript khác (nếu cần) cho xử lý sau khi đăng ký thành công
 }
